a736985d5b0d21d818fbc555fb3a65303b20a5af
André Freitas <60257813+grilokapu@users.noreply.github.com>
2025-08-08 01:14:57 -0300

    small fix

--- src/Tables/trainer_table.c
+++ src/Tables/trainer_table.c
@@ -65,7 +65,7 @@ const struct Trainer gTrainersStevebel[] = {
 	    .items = {},
 	    .doubleBattle = FALSE,
 	    .aiFlags = AI_SCRIPT_CHECK_BAD_MOVE | AI_SCRIPT_SEMI_SMART,  // Flags: 0x3
-	    .partySize = NELEMS(sParty_Rival_Starter_Lab_1),
+	    .partySize = NELEMS(sParty_RivalStarterLab1),
 	    .party = {.NoItemDefaultMoves = sParty_RivalStarterLab1}
 	},
     [TRAINER_RIVAL_STARTER_LAB_2] = {
@@ -77,7 +77,7 @@ const struct Trainer gTrainersStevebel[] = {
 	    .items = {},
 	    .doubleBattle = FALSE,
 	    .aiFlags = AI_SCRIPT_CHECK_BAD_MOVE | AI_SCRIPT_SEMI_SMART,  // Flags: 0x3
-	    .partySize = NELEMS(sParty_Rival_Starter_Lab_2),
+	    .partySize = NELEMS(sParty_RivalStarterLab2),
 	    .party = {.NoItemDefaultMoves = sParty_RivalStarterLab2}
 	},
     [TRAINER_RIVAL_STARTER_LAB_3] = {
@@ -89,7 +89,7 @@ const struct Trainer gTrainersStevebel[] = {
 	    .items = {},
 	    .doubleBattle = FALSE,
 	    .aiFlags = AI_SCRIPT_CHECK_BAD_MOVE | AI_SCRIPT_SEMI_SMART,  // Flags: 0x3
-	    .partySize = NELEMS(sParty_Rival_Starter_Lab_3),
+	    .partySize = NELEMS(sParty_RivalStarterLab3),
 	    .party = {.NoItemDefaultMoves = sParty_RivalStarterLab3}
 	},
 };
