e7e800327975dff924736daec985713b8f668f2a
1RWT16KU1D <darkc8710@gmail.com>
2026-09-01 10:47:17 +0530

    Wrote A Little Debug Script

--- assembly/story_scripts/TheImpactSite/Little_Girl.s
+++ assembly/story_scripts/TheImpactSite/Little_Girl.s
@@ -15,25 +15,15 @@ EventScript_TheImpactSite_LittleGirl:
     setvar 0x8004 0xF
     special 0x62 @Erase Party
 
-    setvar 0x8000 MOVE_CRUSHINGBLOW
-    setvar 0x8001 MOVE_DEFOG
-    setvar 0x8002 MOVE_TELEPORT
-    setvar 0x8003 MOVE_SWEETSCENT
-    setvar 0x8004 5
-    setvar 0x8005 1
-    setvar 0x8006 31
-    setvar 0x8007 31
-    setvar 0x8008 31
-    setvar 0x8009 31
-    setvar 0x800A 31
-    setvar 0x800B 31
-    givepokemon SPECIES_MASTERHOP 40 ITEM_NONE 0 1 0
+    addvar 0x402F 0x1
+    givepokemon 0x402F 40 ITEM_NONE 0 1 0
 
     additem ITEM_BICYCLE 1
     additem ITEM_TM101 1
     additem ITEM_TM102 1
     additem ITEM_TM110 1
 
+    bufferpokemon 0x0 0x402F
     msgbox gText_TheImpactSite_LittleGirl MSG_NORMAL
     release
     end
